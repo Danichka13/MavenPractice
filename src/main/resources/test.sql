71ae530e73a8c38a4c98e5a301dafe8917b3b65c
Danichka13 <99400335+Danichka13@users.noreply.github.com>
2022-04-10 23:43:14 +0400

    Delete src directory

--- src/main/resources/test.sql
+++
@@ -1,3 +0,0 @@
-insert into mydntest.users (name, age, email) values('Danya', 19, 'b.d.v.02@mail.ru');
-insert into mydntest.users (name, age, email) values('Steve', 23, 'b.d.v.02@mail.ru');
-SELECT * FROM mydntest.users;
